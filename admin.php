61e57f32c56104a2aed0c4b46b700b5464bea916
shukurani <shukuraniyvan@gmail.com>
2025-02-01 16:10:14 +0200

    puu

--- admin.php
+++ admin.php
@@ -14,11 +14,11 @@
                 <form action="connection.php" method='POST'>
                     <h2>LOG IN</h2>  
                     <div class="inputbox">
-                        <input type="email" name="email" required>
+                        <input type="email" name="email"  placeholder="Enter your email" required>
                         <label>Email</label>
                     </div>
                     <div class="inputbox">
-                        <input type="password" id="password" name="pass" required>
+                        <input type="password" id="password" name="pass" placeholder="Enter your password" required>
                         <label>Password</label>
                     </div>
                     <button type='submit' name='adminsub'>LOG IN</button>
